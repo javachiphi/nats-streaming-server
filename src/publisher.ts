12d3bf1ec7110023aebb1b52b47b9978a4448429
Sophie Y <javachiphi@gmail.com>
2024-02-09 13:43:33 +0800

    refactor: publisher class & subclass

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 const stan = nats.connect('ticketing', 'abc', {
@@ -8,14 +9,14 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
-
-  // 3rd argument is a callback function
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
